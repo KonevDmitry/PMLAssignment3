5e087d132caa992b5c965b187a0dcee84f1fb430
DmitryKonev <Konev1999D@mail.ru>
2021-03-10 17:17:07 +0300

    changed hyperparameters to score 47.99

--- runner.py
+++ runner.py
@@ -11,6 +11,6 @@ with open('test.ru.txt') as f:
 with open('answer.txt', 'w+') as f:
     for i, val in tqdm.tqdm(enumerate(test), total=len(test)):
         inputs = tokenizer.encode(val, return_tensors="pt")
-        outputs = model.generate(inputs, max_length=90, min_length=70, num_beams=7, early_stopping=True)
+        outputs = model.generate(inputs, max_length=70, num_beams=7, early_stopping=True)
         sequence = (tokenizer.decode(outputs[0])).replace('</s>', '')
         f.write(sequence + "\n") if i != len(test) - 1 else f.write(sequence)
